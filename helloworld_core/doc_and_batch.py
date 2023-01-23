75e9d807cc4a53f3555c1fbc8447b5c5fc7b85d0
John Omernik <darkness@cybermac.local>
2023-01-23 15:50:21 -0600

    Added ability to use different sep and quotes on batch_list_in, defaults to what it was

--- helloworld_core/doc_and_batch.py
+++ helloworld_core/doc_and_batch.py
@@ -121,6 +121,8 @@ def batch_list_in(batchlist, base_query, integration, instance, batchsize=500, d
                   {"name": "integration", "default": "None", "required": "True", "type": "string", "desc": "The integration the query will be submitted to (i.e. splunk, tera, impala)"},
                   {"name": "instance", "default": "None", "required": "True", "type": "string", "desc": "The instance of the previously provided integration that will be used (i.e. prod, dev)"},
                   {"name": "batchsize", "default": "500", "required": "False", "type": "integer", "desc": "The max number of items in a batch."},
+                  {"name": "list_quotes", "default": "single", "required": "False", "type": "string", "desc": "Whether to use single or double quotes on list items. Single is default best for SQLish, double may work better for Splunk types"},
+                  {"name": "list_sep", "default": ", ", "required": "False", "type": "string", "desc": "How the list gets seperated. ', ' is the default and works for SQL IN clauses. Consider ' OR ' for SPLUNK index queries"},
                   {"name": "dedupe", "default": "True", "required": "False", "type": "boolean", "desc": "Deduplicate the list, prior to submitting"},
                   {"name": "remove_none", "default": "True", "required": "False", "type": "boolean", "desc": "Remove null values from the list. "},
                   {"name": "debug", "default": "False", "required": "False", "type": "boolean", "desc": "Turns on debug messages"}
@@ -176,8 +178,16 @@ def batch_list_in(batchlist, base_query, integration, instance, batchsize=500, d
         thisbatch = batchlist[stidx:enidx]
         curs = enidx
         this_len = len(thisbatch)
-        #this_str = str(thisbatch).replace("[", "").replace("]", "")
-        this_str = str(thisbatch)[1:-1]
+        if list_quotes == 'single' and list_sep == ", ":
+            this_str = str(thisbatch)[1:-1]  # just due the default list separator
+        elif list_quotes == "single":
+            this_str = f"'{list_sep}'".join(thisbatch)
+            this_str = f"'{this_str}'"
+        elif list_quotes == "double":
+            this_str = f'"{list_sep}"'.join(thisbatch)
+            this_str = f'"{this_str}"'
+        else:
+            print("Error must use single or double as your list_quotes")
 
         print("Batch: %s - %s - %s" % (stidx, enidx, this_len))
         this_query = base_query.replace("~~here~~", this_str)
