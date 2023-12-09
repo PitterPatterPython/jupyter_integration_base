5929d82d93d135f31a7a771136679818ec2faceb
Rob D'Aveta <rob.daveta@gmail.com>
2023-12-08 22:40:42 -0500

    complete refactor of how uninstall and install of integrations occurs

--- updater_core/updater_full.py
+++ updater_core/updater_full.py
@@ -1,10 +1,10 @@
-from ipywidgets import Button, HBox, VBox, Label, Layout, Output
+from ipywidgets import HBox, VBox, Label, Layout, Output
 from IPython.display import display
 from IPython.core.magic import magics_class, line_cell_magic
-import textwrap
 import jupyter_integrations_utility as jiu
 from addon_core import Addon
-from updater_core.styles import InstallIntegrationButton
+from updater_core.updater_utils.styles import ButtonMaker
+from updater_core.updater_utils.helpers import uninstall_integration, install_integration, cleanup_install, create_load_script
 
 @magics_class
 class Updater(Addon):
@@ -13,78 +13,96 @@ class Updater(Addon):
         super(Updater, self).__init__(shell, debug=False)
         self.debug = debug
         self.output = Output()
+        self.repos = {}
+        self.repos_to_ignore = ["jupyter_integration_base", "qgrid", "jupyter_dummy"]
     
     def customHelp(self, curout):
         if "integrations_cfg" not in self.ipy.user_ns:
             jiu.display_error("Integrations config file isn't loaded into your environment. Try \
                 restarting your kernel, or contact an admin.")
-        else:
-            repos = self.ipy.user_ns["integrations_cfg"]["repos"]
+        else:            
+            self.repos = self.ipy.user_ns["integrations_cfg"]["repos"]
             
             loaded_integrations = list(self.ipy.user_ns["jupyter_loaded_integrations"].keys())
             loaded_integrations = [f"jupyter_{integration}" for integration in loaded_integrations] #<-- consider changing this wherever it's originally created
             loaded_integrations.sort()
             
-            available_integrations = list(repos.keys())
+            
+            available_integrations = list(self.repos.keys())
+            available_integrations = [i for i in available_integrations if i not in self.repos_to_ignore]
             available_integrations = list(set(available_integrations).difference(loaded_integrations)) # Get rid of items that are already loaded
             available_integrations.sort()            
 
             ######################################################
-            # Create the table for Loaded/Installed Integrations #
+            # Create the table for Loaded Integrations #
             ######################################################
             loaded_rows = []
             title_row = HBox([Label(value="Installed Integrations", style=dict(font_weight="bold", font_size="18px"))])
+            subtitle_row = HBox([
+                Label(value="These integrations are already installed and loaded in your environment, and can be updated", 
+                      style=dict(font_size="14px"), 
+                      layout=Layout(display="flex", width="100%"))
+                ])
             header_row = HBox([Label(value="Integration", style=dict(font_weight="bold"), layout=Layout(display="flex", width="20%", justify_content="flex-start")),
                                Label("Repo URL", style=dict(font_weight="bold"), layout=Layout(display="flex", width="60%", justify_content="flex-start")),
                                Label("", layout=Layout(display="flex", width="10%"))]
                               )
             loaded_rows.append(title_row)
+            loaded_rows.append(subtitle_row)
             loaded_rows.append(header_row)
             
             for integration in loaded_integrations:
 
-                install_button = InstallIntegrationButton(value = integration,
-                                                  description = "Re-install",
-                                                  button_style = "info",
-                                                  layout = Layout(
-                                                      display = "flex",
-                                                      width="10%",
-                                                      justify_content = "center"
-                                                      
-                                                  ))
+                install_button = ButtonMaker(value = integration,
+                                             description = "Update",
+                                             button_style = "info",
+                                             origin = "loaded",
+                                             layout = Layout(
+                                                 display = "flex",
+                                                 width="10%",
+                                                 justify_content = "center"
+                                                 ))
                 install_button.on_click(self.on_click)
                 
                 loaded_rows.append(HBox([Label(integration.replace("jupyter_", "").capitalize(), layout=Layout(display="flex", width="20%", justify_content="flex-start")), 
-                                         Label(repos[integration]["repo"], layout=Layout(display="flex", width="60%", justify_content="flex-start")), 
+                                         Label(self.repos[integration]["repo"], layout=Layout(display="flex", width="60%", justify_content="flex-start")), 
                                          install_button]
                                       )
                                  )
+                
             ###############################################
             # Create the table for Available integrations #
             ###############################################
             available_rows = []
             title_row = HBox([Label(value="Available Integrations", style=dict(font_weight="bold", font_size="18px"))])
+            subtitle_row = HBox([
+                Label(value="These integrations can be loaded into your current environment (they won't be installed).", 
+                      style=dict(font_size="14px"), 
+                      layout=Layout(display="flex", width="100%", flex_flow="row wrap"))
+                ])
             header_row = HBox([Label(value="Integration", style=dict(font_weight="bold"), layout=Layout(display="flex", width="20%", justify_content="flex-start")),
                                Label("Repo URL", style=dict(font_weight="bold"), layout=Layout(display="flex", width="60%", justify_content="flex-start")),
                                Label("", layout=Layout(display="flex", width="10%"))]
                               )
             available_rows.append(title_row)
+            available_rows.append(subtitle_row)
             available_rows.append(header_row)
             
             for integration in available_integrations:
-                install_button = InstallIntegrationButton(value = integration,
-                                                  description = "Install",
-                                                  button_style = "info",
-                                                  layout = Layout(
-                                                      display = "flex",
-                                                      width = "10%",
-                                                      justify_content = "center"
-                                                  ))
+                install_button = ButtonMaker(value = integration,
+                                             description = "Load",
+                                             button_style = "info",
+                                             origin = "available",
+                                             layout = Layout(
+                                                 display = "flex",
+                                                 width = "10%",
+                                                 justify_content = "center"
+                                                 ))
                 
                 install_button.on_click(self.on_click)
                 
                 available_rows.append(HBox([Label(integration.replace("jupyter_", "").capitalize(), layout=Layout(display="flex", width="20%", justify_content="flex-start")), 
-                                       Label(repos[integration]["repo"], layout=Layout(display="flex", width="60%", justify_content="flex-start")), 
+                                       Label(self.repos[integration]["repo"], layout=Layout(display="flex", width="60%", justify_content="flex-start")), 
                                        install_button]
                                       )
                                  )
@@ -100,9 +118,57 @@ class Updater(Addon):
             return output
     
     def on_click(self, b):
-        with self.output:
-            print(f"\r{' ' * 50}", end="") # Overwrite longer lines of text
-            print(f"\r{b.value}", end="")
+        self.output.clear_output()
+        
+        try:
+            with self.output:
+                # First attempt to uninstall
+                jiu.display_info(f"Force-uninstalling `{b.value}...")
+                uninstall_code = uninstall_integration(b.value)                
+                
+                # Attempt to install the integration if the uninstall was successful
+                if uninstall_code == 0:
+                    jiu.display_info(f"Attempting to install `{b.value}`...")
+                    repo_url = self.repos[b.value]["repo"]
+                    proxies = self.ipy.user_ns["integrations_cfg"]["proxies"]
+                    install_code = install_integration(b.value, repo_url, proxies)
+                    
+                    if install_code == 0:
+                        self.output.clear_output()
+                        jiu.display_success(f"Successfully installed `{b.value}`!")
+                        
+                        load_script = create_load_script(b.value.replace("jupyter_", ""))
+
+                        if b.origin == "available":
+                            jiu.display_warning(f"{b.value} is installed to your environment now, but you need to re-load your kernel and then run the code block below.")
+                            jiu.displayMD(f"\n```\n{load_script}```")
+                            
+                        elif b.origin == "loaded":
+                            jiu.display_warning("Copy and run the code block below to re-load")
+                            jiu.displayMD(f"\n```\n{load_script}```")
+                            
+                        else:
+                            jiu.display_error("You clicked on a button that has no `origin` property set and that's bad.")
+                            
+                    else:
+                        self.output.clear_output()
+                        jiu.display_error(f"Error running install of {b.value}. Error code: `{install_code}`")
+                    
+                    # Cleanup the install regardless of the outcome
+                    if self.debug:
+                        jiu.display_info(f"Cleaning up after install of `{b.value}`...")
+                    
+                    cleanup_install(b.value)
+                    
+                    if self.debug:
+                        jiu.display_info(f"Finished cleanup...")                    
+                
+                else:
+                    jiu.display_error(f"Uninstall of {b.value} failed. This should never happen: contact an admin.")
+            
+        except Exception as ex:
+            cleanup_install(b.value)
+            jiu.display_error(ex)
     
     # This is the magic name.
     @line_cell_magic
@@ -115,7 +181,6 @@ class Updater(Addon):
             line_handled = self.handleLine(line)
         
             if not line_handled: # We based on this we can do custom things for integrations. 
-        
                 if line.lower().find("functions") == 0:
                     newline = line.replace("functions", "").strip()
         
